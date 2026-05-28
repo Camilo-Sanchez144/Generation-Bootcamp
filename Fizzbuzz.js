2268d2cba3c7252a5bbb3514ce726bf1717d360f
Camilo <camilos0704@gmail.com>
2026-05-28 16:22:08 -0500

    master

---
+++ Fizzbuzz.js
@@ -0,0 +1,92 @@
+
+const prompt = require('prompt-sync')();
+
+let saldo = 500000
+let pin = "MyPassword123*"
+let menuOpciones = prompt('hola')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+for (let i = 1; i <= 1000; i++) {
+  
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log("FizzBuzz");  
+
+  } else if (i % 3 === 0) {
+    console.log("Fizz");
+
+  } else if (i % 5 === 0) {
+    console.log("Buzz");
+
+  } else {
+    console.log(i);
+  }
+}*/
---
+++ variablesYTiposdedatos.js
@@ -0,0 +1,64 @@
+const prompt = require('prompt-sync')()
+
+// const nombre = "Camilo"
+// const miEdad = 22
+// const esEstudiante = true
+
+// let sinValor
+// const valorNulo = null
+// console.log(sinValor, valorNulo)
+
+// let edad = parseInt(prompt('Ingrese su año de nacimiento: '))
+
+// edad = 2026 - edad
+// console.log('su edad es: '+ edad)
+
+// let num1 = parseInt(prompt('Ingrese numero 1: '))
+// let num2 = parseInt(prompt('Ingrese numero 2: '))
+// let suma = num1+num2
+// console.log('La suma de ambos es: '+ suma)
+
+// let edad = parseInt(prompt('Ingrese su edad: '))
+// if(edad >= 18){
+//     console.log("Puedes entrar")
+// }
+// else{
+//     console.log('No puedes entrar'); 
+// }
+
+// let numero = parseInt(prompt('Ingrese un numero: '))
+// if(numero > 0 ){
+//     console.log('El numero es positivo')
+// }
+// else if(numero < 0){
+//     console.log('El numero es negativo');
+// }
+// else if(numero==0){
+//     console.log('Es cero');
+// }
+// let numero = parseInt(prompt('Ingrese un numero: '))
+// let contador = 0
+// while (contador <= numero){
+//     contador = contador + 1
+//     console.log(contador)
+// }
+// let userContrasena
+// let contrasenaCorrecta = 1234
+// while(userContrasena != contrasenaCorrecta){
+//     userContrasena = prompt('Escribe tu contraseña: ')
+//     if(userContrasena != contrasenaCorrecta){
+//         console.log('contraseña incorrecta')
+//     }
+//     else if(userContrasena == contrasenaCorrecta){
+//         console.log('contraseña correcta')
+//     }
+// }
+
+// for (let i = 1; i<=10;i++){
+//     console.log(i)
+// }
+// let numero = parseInt(prompt('Ingresa el numero: '))
+// for(let i = 1; i<=10; i++){
+//     console.log(`${numero}x${i}=${numero*i}`)
+// }
+
